6d7eea4fd62661dc419ff695b77ae851a24161b8
Abdullah <mukaddim1998@gmail.com>
2021-05-04 20:11:22 -0300

    Added decorations and Image model

--- mysite/shopify_images/decorators.py
+++ mysite/shopify_images/decorators.py
@@ -12,7 +12,10 @@ def unathenticated_user(view_func):
     return wrapper_func
 
 
-def allowed_users(allowed_roles=[]):
+def allowed_users(allowed_roles=None):
+    if allowed_roles is None:
+        allowed_roles = []
+
     def decorator(view_func):
         def wrapper_func(request, *args, **kwargs):
             group = None
--- mysite/shopify_images/models.py
+++ mysite/shopify_images/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    file_name = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='user_uploads')
+    private = models.BooleanField(default=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.file_name
