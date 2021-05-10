7606fde4f5ed474cd9ff6a052efa96a114ffed05
Abdullah <mukaddim1998@gmail.com>
2021-05-09 22:16:17 -0300

    Added test cases

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-*z(a(atfog6*6-72azqp4bn-mf(ce%4g+=1(*okmb-ru510emw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '127.0.0.2']
+ALLOWED_HOSTS = ['127.0.0.1', '127.0.0.2', 'testserver']
 
 # Application definition
 
--- mysite/shopify_images/tests.py
+++ mysite/shopify_images/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from shopify_images.models import Image
 
-# Create your tests here.
+
+class ImageClassTest(TestCase):
+    def setUp(self):
+        Image.objects.create(file_name="abc", privacy="Private", image="image.png")
+
+    def test_filename(self):
+        abc = Image.objects.get(file_name="abc")
+        self.assertEqual(abc.file_name, "abc")
+        self.assertNotEqual(abc.file_name, "abd")
+
+    def test_privacy(self):
+        abc = Image.objects.get(file_name="abc")
+        self.assertEqual(abc.privacy, "Private")
+        self.assertNotEqual(abc.privacy, "Public")
+        self.assertNotEqual(abc.privacy, "abc")
+
+    def test_image(self):
+        abc = Image.objects.get(file_name="abc")
+        self.assertEqual(abc.image, "image.png")
+        self.assertNotEqual(abc.image, "123.png")
+
+    def test_fileupload(self):
+        c = Client()
+        with open('media/uploads/image.png') as fp:
+            c.post('uploads/', {'file_name': 'fred', 'privacy': 'Public'})
+        self.assertEqual(fp.name, "media/uploads/image.png")
+
